6a2de3b8bb110cc07a398d7e69d3322beddc9d12
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2016-12-17 18:01:01 +0000

    added source to notes

--- linux/pyxhook.py
+++ linux/pyxhook.py
@@ -4,6 +4,8 @@
 #
 #    Copyright (C) 2008 Tim Alexander <dragonfyre13@gmail.com>
 #
+#    View the repo: https://github.com/JeffHoogland/pyxhook
+#    
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
