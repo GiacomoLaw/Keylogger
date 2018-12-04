d48701add9f43e312fc0c1568df77dd8910f97de
Bahram Aghaei <aghaee.bahram@gmail.com>
2018-12-04 09:44:29 +0000

    add package and setup.py file (#63)
    * add package and setup.py file
    
    * Update __version__.py

---
+++ linux/keylogger/__version__.py
@@ -0,0 +1,7 @@
+__title__ = 'keylogger'
+__description__ = ' A simple keylogger for Windows, Linux and Mac '
+__url__ = 'https://simple-keylogger.github.io'
+__version__ = '0.1'
+__author__ = 'Giacomo Lawrance'
+__author_email__ = 'thenerdystudent@gmail.com'
+__license__ = 'MIT'
---
+++ linux/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import os
+import re
+import sys
+
+from codecs import open
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'xlib',
+]
+
+about = {}
+with open(os.path.join(here, 'keylogger', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    install_requires=requires,
+    license=about['__license__'],
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points = {
+        'console_scripts':
+        ["keylogger = keylogger.keylogger:main"],
+        },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
